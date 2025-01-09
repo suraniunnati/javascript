5951f2859b9b5e0bc846bfc491f77214345b6117
suraniunnati <suraniunnati5@gmail.com>
2025-01-09 11:41:23 +0530

    web3

--- website_3/js/cart.js
+++ website_3/js/cart.js
@@ -1,4 +1,41 @@
 function Cart(){
+    fetch(`http://localhost:3000/cart`)
+    .then((r)=>r.json())
+    .then((res)=>{
+        document.getElementById("cartbox").innerHTML=view(res)
+    })
+    .catch((err)=>{console.log(err)})
 
+}
+function view(arr){
+    return arr.map((ele)=>{
+        return `  <div class="flex border">
+         <div id="img">
+         <img src="${ele.img}">
+         </div>
+        <div id="text">
+        <p id="title">${ele.title}</p>
+
+        <div>
+         <button onclick="Addcart(${ele.id} , ${ele.queinty} , 'dec')" id="Add">-</button>
+        <input type="text" value="${ele.queinty}" disabled id="quan">
+        <button onclick="Addcart(${ele.id} , ${ele.queinty} , 'inc')" id="Add">+</button>
+        <br>
+        <button onclick="del(${ele.id})" id="delete"> Remove </button>
+        </div>
+
+        <div id="">
+         <span id="pr">Price : ₹${ele.mainPrice *  ele.queinty}</span><br>
+           <span id="pr">Discount : -₹${(ele.mainPrice * ele.off/100 *  ele.queinty).toFixed(0)}</span>
+           <br>
+           <span id="pr">Shipping Fee : FREE</span>
+        </div>
+        <div id="itemTotal">
+            <span id="price"> ₹${ele.price * ele.queinty}</span>
+            <p id="tex">Inclusive of all the applicable</p>
+            <p id="tex">taxes</p>
+        </div>
+        </div>`
+    })
 }
 Cart()
--- website_3/js/singlepage.js
+++ website_3/js/singlepage.js
@@ -1,26 +1,38 @@
-function singlepage(){
-    let data=new URLSearchParams(window.location.search);
-   let id = data.get("id");
-   console.log(id);
+function singlepage() {
+    let data = new URLSearchParams(window.location.search);
+    let id = data.get("id");
+    console.log(id);
 
-fetch(`http://localhost:3000/product/${id}`)
-.then((r)=>{
-    return r.json()
-})
-.then((res)=>{
-    console.log(res)
-    document.getElementById("singlpage").innerHTML= show(res)
-   
-})
+    fetch(`http://localhost:3000/product/${id}`)
+        .then((r) => {
+            return r.json()
+        })
+        .then((res) => {
+            console.log(res)
+            document.getElementById("singlpage").innerHTML = show(res)
+            document.getElementById("cart").addEventListener("click",()=>{
+                AddCart(res)
+              })
+
+        })
 
 }
 
-function show(arr){
-    return `<div class="flex border w-[80%] m-[auto]">
-    <div class="w-[40%]"><img src="${arr.img}"></div>
-            <div><h2 class="font-medium mb-2 text-xl">${arr.title}</h2>
-                 <h5>
-                  <i class="fa-solid fa-star fa-2xs"></i>
+function show(arr) {
+    return `<div class="flex border w-[80%] m-[auto] bg-white p-3">
+    <div class="w-[35%] flex items-center border-e">
+    <div class="w-[20%]  flex flex-col justify-center items-center ">
+     <img src="${arr.img}" class="w-[50px] border  mt-2 p-1">
+     <img src="${arr.img1}" class="w-[50px] border  mt-2 p-1">
+      <img src="${arr.img2}" class="w-[50px] border  mt-2 p-1">
+       <img src="${arr.img3}" class="w-[50px] border  mt-2 p-1">
+        <img src="${arr.img4}" class="w-[50px] border  mt-2 p-1">
+    </div>
+     <div> <img src="${arr.img}"></div>
+   </div>
+            <div class="ps-5 w-[70%]"><h2 class="font-medium mb-2 text-xl">${arr.title}</h2>
+             <h5>
+           <i class="fa-solid fa-star fa-2xs"></i>
            <i class="fa-solid fa-star fa-2xs"></i>
            <i class="fa-solid fa-star fa-2xs"></i>
            <i class="fa-solid fa-star fa-2xs"></i>
@@ -31,9 +43,57 @@ function show(arr){
                  </h5>   
                   <h4 class="mt-2"><span class="text-stone-400 line-through">MRP:₹${arr.mrp}</span> <span class="text-lg font-medium">₹${arr.price}</span> <span class="text-stone-300">|</span> <span class="text-lime-700 font-medium">${arr.off}%Off</span></h4>
                   <h4 class="text-stone-400">inclusive of all taxes</h4>
+                  <div class="flex h-[100px] mt-5">
+                     <div class="w-[50%] pt-5 border-e text-center"><button class="text-white h-[50px] w-[200px] bg-[#E80071]" id="cart">Add to Bag</button></div>
+                      <div class="w-[50%] pt-3 ps-5"><p class="mb-2"><i class="fa-solid fa-location-dot"></i> Delivery Options</p><input type="text" placeholder="Enter Pincode" class="border h-[40px] ps-1"><button class="text-white h-[40px] w-[80px] bg-[#E80071]">Check</button></div>
+                  </div>
             </div>
             </div>
     `
 }
+
+
+function AddCart(res){
+
+    fetch(`http://localhost:3000/cart?id=${res.id}`)
+    .then((r)=>{return r.json()})
+    .then((Res)=>{
+         if(Res.length > 0){
+          Swal.fire({
+            position: "center",
+            icon: "error",
+            title: "Item is Already Present in Cart....",
+            showConfirmButton: false,
+            timer: 1500
+          })
+         }
+         else{
+          fetch(`http://localhost:3000/cart`,
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({...res,queinty: 1})
+            })
+            .then((r)=>r.json())
+            .then((res)=>console.log(res),
+            Swal.fire({
+              position: "center",
+              icon: "success",
+              title: "Item is Successfully add in Cart...",
+              showConfirmButton: false,
+              timer: 1500
+            })
+          )
+            .catch((err)=>
+              console.log(err),
+           
+          ) 
+         }
+    })
+    
+     
+    }
 
 singlepage()
